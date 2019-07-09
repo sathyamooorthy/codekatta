d7bf4e19413143b2411a13c735409ef10dd087dc
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-07-09 15:59:32 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int original,reverse=0,num,remainder;
+scanf("%d",&num);
+original=num;
+while(num!=0)
+{
+	remainder = num %10;
+	reverse = reverse *10 + remainder;
+	num=num/10;
+}
+if(original== reverse)
+{
+	printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+
+}
