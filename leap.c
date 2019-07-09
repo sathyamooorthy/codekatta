8880cf9c9b7ad73b853778b397144386081a50ff
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-07-09 16:26:40 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%4==0 || n%400==0 || n%100==0)
+    {
+    printf("yes");
+    }
+    else
+    {
+    printf("no");
+    }
+}
