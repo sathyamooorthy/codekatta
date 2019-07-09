690c5cb3fccb35f232210aadf8506bf3df0d34b9
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-07-09 12:13:58 +0530

    Update EvenInRange.c

--- EvenInRange.c
+++ EvenInRange.c
@@ -3,6 +3,8 @@ int main()
 {
     int a,b,i;
     scanf("%d%d",&a,&b);
+    if(a>0)
+    {
 for(i=a;i<b-1;)
 {
     i++;
@@ -10,5 +12,6 @@ for(i=a;i<b-1;)
     {
         printf("%d ",i);
     }
+}
 }
 }
