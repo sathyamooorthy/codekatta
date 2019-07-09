e3ecb1dad91ec91494c63fbf45805bd485f197ec
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-07-09 13:04:20 +0530

    Create EvenOdd.c

---
+++ EvenOdd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    scanf("%d",&i);
+    if (i>=0)
+    {
+        if(i%2==0)
+        {
+        printf("Even");
+        }
+        else if(i%2==1)
+        {
+        printf("Odd");
+        }
+    }
+    else
+    {
+        printf("invalid");
+    }
+  
+    
+}
