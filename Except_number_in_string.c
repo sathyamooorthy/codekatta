98d8d6145b9bf9b5cae8560532973b27998c74f4
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-06 18:26:52 +0530

    Update Except_number_in_string.c

--- Except_number_in_string.c
+++ Except_number_in_string.c
@@ -8,8 +8,12 @@ int main()
     gets(a);
     for(int i=0; a[i] !=0;i++)
     {
-        
-        
+        if((a[i] < 'a' || a[i] > 'z') && (a[i] < 'a' || a[i] > 'z') && (a[i] < '0' || a[i] > '9'))
+        {
+            if(a[i] == ' ')
+              continue;
+            count++;
+        }
     }
     printf("%d", count);
     return 0;
