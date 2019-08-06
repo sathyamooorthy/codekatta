cba0c270dd30bda0d04f14dd13f3abbbc7eee327
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-06 15:48:41 +0530

    Create Array_with_index.c

---
+++ Array_with_index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, n ,a[10], min=0, sort=0;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+    printf("%d %d\n",a[i],i);
+      
+  }
+  
+  
+}
