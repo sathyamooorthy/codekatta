23786ca7dff0eabb9c9d31728753460d3875674d
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 08:01:18 +0530

    Create even_or_odd_in_diff.c

---
+++ set7/even_or_odd_in_diff.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n1,n2,sum;
+    scanf("%d%d",&n1,&n2);
+    sum=n1-n2;
+    if(sum%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+        
+}
