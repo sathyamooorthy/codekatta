eead5f9feec42983a4d9d159c4c21ac245cd9038
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 07:41:53 +0530

    Create prime.c

---
+++ set7/prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i;
+    int number;
+    unsigned char flag=0;
+    scanf("%d",&number);
+    for(i=2; i<=(number/2); i++)
+    {
+        if(number%i ==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+        printf("yes");
+    else
+        printf("no");
+ 
+    return 0;
+}
