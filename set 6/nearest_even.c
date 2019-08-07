8c36c3206d535a138c32d18f700eb9598713a070
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 06:49:03 +0530

    Create nearest_even.c

---
+++ set 6/nearest_even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i,sum=0;
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("%d",i);
+    }
+    else
+    {
+        printf("%d",i-1);
+    }
+    
+}
