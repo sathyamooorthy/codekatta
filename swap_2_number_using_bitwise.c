d3b141a01398bb12dcf92af2e12d5c6e29a8ec69
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 09:27:39 +0530

    Update swap_2_number_using_bitwise.c

--- swap_2_number_using_bitwise.c
+++ swap_2_number_using_bitwise.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-void swap(int*, int *);
-void main()
+
+int main()
 {
-    int num1, num2;
-    
-    scanf("%d %d", &num1, &num2);
-    swap(&num1, &num2); 
-    printf("%d %d", num1,num2);
-  }
-void swap(int *x, int *y)
-{
-    *x = *x ^ *y;
-    *y = *x ^ *y;
-    *x = *x ^ *y;
+    int n1,n2;
+    scanf("%d %d", &n1, &n2);
+    n1 = n1 ^ n2;
+    n2 = n1 ^ n2;
+    n1 = n1 ^ n2;
+    printf("%d %d", n1, n2);
+       return 0;
 }
+
 
