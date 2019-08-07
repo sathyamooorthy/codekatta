706a0c0627ab0bf444e6dc35e28907f8d13dfe09
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 07:15:53 +0530

    Create even_or_odd_after_Mul.c

---
+++ even_or_odd_after_Mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n1,n2,mul;
+    scanf("%d%d",&n1,&n2);
+    mul=n1*n2;
+    if(mul%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    
+        
+}
