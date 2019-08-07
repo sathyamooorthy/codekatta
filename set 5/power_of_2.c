799b3c2e0631e93b7bd6a06a6266508fe32f83ca
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 06:31:33 +0530

    Create power_of_2.c

---
+++ set 5/power_of_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
