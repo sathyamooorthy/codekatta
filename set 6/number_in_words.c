7f63fadaa78f03c7eb6d142e694345fcb902a9c0
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 06:40:06 +0530

    Create number_in_words.c

---
+++ set 6/number_in_words.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    switch(i)
+    {
+        case 1: printf("One"); break;
+        case 2: printf("Two"); break;
+        case 3: printf("Three"); break;
+        case 4: printf("Four"); break;
+        case 5: printf("Five"); break;
+        case 6: printf("Six"); break;
+        case 7: printf("Seven"); break;
+        case 8: printf("Eight"); break;
+        case 9: printf("Nine"); break;
+        case 10: printf("Ten"); break;
+        
+    }
+}
