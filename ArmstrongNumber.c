99130a9c743ebeffc38bb45255c13e79f5005752
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-08 08:11:58 +0530

    Update ArmstrongNumber.c

--- ArmstrongNumber.c
+++ ArmstrongNumber.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
+#include<math.h>
  
 int main()
 {
-    int number, sum=0, rem=0,tempNumber;
+    int number, sum=0, rem=0,tempNumber,count=0,check;
  
   
     scanf("%d", &number);
  
     tempNumber=number;
+    check=tempNumber;
+    while(number !=0)
+    {
+        count++;
+        number=number/10;
+    }
+    printf("%d\n", count);
  
     while(tempNumber!=0)
     {
         rem=tempNumber%10;
-        sum=sum + (rem*rem*rem);
+        sum=sum + pow(rem,count);
         tempNumber/=10;
     }  
+   
+   printf("%d\n",sum);
+   printf("%d\n",check);
 
-    if(sum==number)  
+    if(sum==check)  
         printf("yes");
     else
         printf("no");
