c8540c6c37f261e691af0ad8075d787171e92763
Sathyamoorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-11 19:58:26 +0530

    Create numberInList.c

---
+++ set 8/numberInList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+    
+    int a,b,n;
+    scanf("%d%d%d",&n,&a,&b);
+    if(n>a && n<b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
