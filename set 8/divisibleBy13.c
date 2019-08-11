271b3f33748872e6b2d2dee52d02df089d20609b
Sathyamoorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-11 21:08:45 +0530

    Create divisibleBy13.c

---
+++ set 8/divisibleBy13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    if(a%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+    return 0;
+}
